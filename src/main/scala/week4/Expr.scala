673fbc3bf04311a6417118af297d7cb233d6abef
JuanSe Dussan <juandussan@seven4n.com>
2019-03-13 10:32:14 -0500

    Delegate implementation

--- src/main/scala/week4/Expr.scala
+++ src/main/scala/week4/Expr.scala
@@ -1,46 +1,16 @@
 package week4
 
 trait Expr {
-  // Classification
-
-  //  def isNumber: Boolean
-  //
-  //  def isSum: Boolean
-
-  // Accessor
-  def numValue: Int
-
-  def leftOp: Expr
-
-  def rightOp: Expr
-
-  def eval(e: Expr): Int = {
-    if (e.isInstanceOf[Number]) e.numValue
-    else if (e.isInstanceOf[Sum])
-      eval(e.asInstanceOf[Sum].leftOp) +
-        eval(e.asInstanceOf[Sum].rightOp)
-    else throw new Error("Unknown expression" + e)
-  }
+  def eval: Int
 
 }
 
 class Number(n: Int) extends Expr {
-  // Accessor
-  def numValue: Int = n
-
-  def leftOp: Expr = throw new Error("Number.leftOp")
-
-  def rightOp: Expr = throw new Error("Number.rightOp")
+  def eval: Int = n
 
 }
 
 class Sum(e1: Expr, e2: Expr) extends Expr {
-  // Accessor
-  def numValue: Int = throw new Error("Sum.numValue")
-
-  def leftOp: Expr = e1
-
-  def rightOp: Expr = e2
-
+  def eval: Int = e1.eval + e2.eval
 }
 
