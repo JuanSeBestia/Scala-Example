1e08efcf6b933e848912c0d39690d328380e56d1
JuanSe Dussan <juandussan@seven4n.com>
2019-03-13 10:14:46 -0500

    List and Covariants

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -1,11 +1,14 @@
 package week4
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
 
   def head: T
 
   def tail: List[T]
+
+  def prepend[U >: T](elem: U): List[U] = new Const[U](elem, this)
+
   def nth(index: Int): T =
     if (index < 0) throw new IndexOutOfBoundsException()
     else if (index == 0) head
@@ -16,7 +19,7 @@ class Const[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
 
   def head: Nothing = throw new NoSuchElementException("Nil.head")
@@ -24,3 +27,13 @@ class Nil[T] extends List[T] {
   def tail: Nothing = throw new NoSuchElementException("Nil.head")
 }
 
+object List {
+  // List(1,2) == List.apply(1,2)
+  def apply[T](x1: T, x2: T): List[T] = new Const[T](x1, new Const[T](x2, Nil))
+
+  def apply[T](): List[T] = new Nil[T]
+}
+
+object test {
+  val x: List[String] = Nil
+}
